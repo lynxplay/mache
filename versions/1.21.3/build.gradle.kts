723cbf6535c945ac68e672e0317ba576d047806f
Bjarne Koll <git@lynxplay.dev>
2024-11-22 00:38:59 +0100

    1.21.3

--- versions/1.21.3/build.gradle.kts
+++ versions/1.21.3/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 val generateReportsProperty = providers.gradleProperty("generateReports")
 mache {
     minecraftVersion = "1.21.3"
-    minecraftJarType = MinecraftJarType.SERVER
+    minecraftJarType = MinecraftJarType.CLIENT
 
     repositories.register("sonatype snapshots") {
         url = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
