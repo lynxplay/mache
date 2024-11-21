9c10980e5befcb46152cfaa617a03e2326cbba12
Bjarne Koll <git@lynxplay.dev>
2024-11-22 00:38:59 +0100

    Client patches

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    gradlePluginPortal()
+    maven("https://repo.papermc.io/repository/maven-releases/") {
+        mavenContent { releasesOnly() }
+    }
+}
+
+dependencies {
+    implementation("io.papermc.sculptor:sculptor-version:1.0.13")
+}
