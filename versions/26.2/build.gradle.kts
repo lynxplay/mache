925c6062129e9dbac72bd97c2f645612642984f9
Bjarne Koll <git@lynxplay.dev>
2024-11-22 00:38:59 +0100

    Client patches

--- versions/26.2/build.gradle.kts
+++ versions/26.2/build.gradle.kts
@@ -1,13 +1,13 @@
 import io.papermc.sculptor.shared.util.MinecraftJarType
 
 plugins {
-    id("io.papermc.sculptor.version") version "2.0.0"
+    id("client-autoconfigure")
 }
 
 val generateReportsProperty = providers.gradleProperty("generateReports")
 mache {
     minecraftVersion = "26.2"
-    minecraftJarType = MinecraftJarType.SERVER
+    minecraftJarType = MinecraftJarType.CLIENT
 
     val args = mutableListOf(
         "--temp-dir={tempDir}",
@@ -29,12 +29,13 @@ mache {
 
 tasks.withType<JavaCompile>().configureEach {
     options.compilerArgs.add("-parameters")
+    options.forkOptions.memoryMaximumSize = "1G"
 }
 
 dependencies {
     codebook("2.0.1-SNAPSHOT")
     decompiler(vineflower("1.12.0"))
-    constants("io.papermc.unpick-definitions:unpick-definitions:26.2-snapshot-6+build.4") // todo bump once https://github.com/FabricMC/unpick/issues/46 is fixed
+    constants("io.papermc.unpick-definitions:unpick-definitions:26.2-pre-4+build.1") // todo bump once https://github.com/FabricMC/unpick/issues/46 is fixed
 }
 
 dependencies {
